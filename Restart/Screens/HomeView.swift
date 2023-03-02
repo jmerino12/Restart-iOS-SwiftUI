1411d8a847a27cab9e479797ef3d04bcd052bed1
Jonathan Meriño B <jonathan.merino@ceiba.com.co>
2023-03-02 08:49:12 -0500

    HomeScreen

--- Restart/Screens/HomeView.swift
+++ Restart/Screens/HomeView.swift
@@ -14,14 +14,39 @@ struct HomeView: View {
     // MARK: - BODY
     var body: some View {
         VStack(spacing: 20) {
-            Text("Home")
-                .font(.largeTitle)
+            // MARK: - HEADER
+            Spacer()
+            
+            ZStack {
+                CircleGroupView(ShapeColor: .gray, ShapeOpacity: 0.1)
+                Image("character-2")
+                    .resizable()
+                    .scaledToFit()
+                .padding()
+            }
+            
+            
+            // MARK: - CENTER
+            Text("The time that leads to mastery is dependent on the intensity of out focus.")
+                .font(.title3)
+                .fontWeight(.light)
+                .foregroundColor(.secondary)
+                .multilineTextAlignment(.center)
+                .padding()
+            // MARK: - FOOTER
+            Spacer()
             
             Button(action: {
                 isOnboardingViewActive = true
             }) {
+                Image(systemName: "arrow.triangle.2.circlepath.circle.fill")
+                    .imageScale(.large)
                 Text("Reset")
-            }
+                    .font(.system(.title3, design: .rounded))
+            }//: BUTTON
+            .buttonStyle(.borderedProminent)
+            .buttonBorderShape(.capsule)
+            .controlSize(.large)
         }//: VSTACK
     }
 }
