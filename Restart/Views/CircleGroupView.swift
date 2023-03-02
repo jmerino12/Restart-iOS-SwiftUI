cd29d79037e46ef270a3e522811fa4cb52b81324
Jonathan Meriño B <jonathan.merino@ceiba.com.co>
2023-03-02 08:49:03 -0500

    HomeScreen

---
+++ Restart/Views/CircleGroupView.swift
@@ -0,0 +1,20 @@
+//
+//  CircleGroupView.swift
+//  Restart
+//
+//  Created by Jonathan Eduardo Meriño Bolívar - Ceiba Software on 2/03/23.
+//
+
+import SwiftUI
+
+struct CircleGroupView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CircleGroupView_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleGroupView()
+    }
+}
