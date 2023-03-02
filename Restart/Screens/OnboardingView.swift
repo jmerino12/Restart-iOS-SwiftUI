bb1bdea4b1bba950a6acee46631b97177c82c3bd
Jonathan Meriño B <jonathan.merino@ceiba.com.co>
2023-03-02 07:56:23 -0500

    Onboarding screen, with custom button swipe

--- Restart/Screens/OnboardingView.swift
+++ Restart/Screens/OnboardingView.swift
@@ -14,16 +14,99 @@ struct OnboardingView: View {
    // MARK: - BODY
     
     var body: some View {
-        VStack(spacing: 20) {
-            Text("Onboarding")
-                .font(.largeTitle)
-            
-            Button(action: {
-                isOnboardingViewActive = false
-            }) {
-                Text("Start")
-            }
-        }//: VSTACK
+        ZStack {
+            Color("ColorBlue")
+                .ignoresSafeArea(.all, edges: .all)
+            VStack(spacing: 20) {
+                // MARK: - HEADER
+                Spacer()
+                
+                VStack(spacing: 0){
+                    Text("Share.")
+                        .font(.system(size: 60))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                    
+                    Text("""
+                        It's not how much we give but
+                        how much love we put into giving.
+                        """)
+                    .font(.title3)
+                    .fontWeight(.light)
+                    .foregroundColor(.white)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal, 10)
+                    
+                }//: HEADER
+                
+                // MARK: - CENTER
+                
+                ZStack {
+                    ZStack {
+                        Circle().stroke(.white.opacity(0.2), lineWidth: 40)
+                            .frame(width: 260, height: 260, alignment: .center)
+                        Circle()
+                            .stroke(.white.opacity(0.2), lineWidth: 80)
+                            .frame(width: 260, height: 260, alignment: .center)
+                    }
+                    
+                    Image("character-1")
+                        .resizable()
+                        .scaledToFit()
+                }
+                
+                Spacer()
+                
+                // MARK: - FOOTER
+                
+                ZStack {
+                // PARTS OF THE CUSTOM BUTTON
+                    
+                // 1. BACKGROUND (STATIC)
+                    Capsule().fill(.white.opacity(0.2))
+                    Capsule().fill(.white.opacity(0.2)).padding(8)
+                // 2. CALL-TO-ACTION (STATIC)
+                    
+                    Text("Get started")
+                        .font(.system(.title3, design: .rounded))
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .offset(x: 20)
+                    
+                // 3. CAPSULE (DYNAMIC WIDTH)
+                    HStack {
+                        Capsule().fill(Color("ColorRed"))
+                            .frame(width: 80)
+                        
+                        Spacer()
+                    }
+                    
+                // 4. CIRCLE (DRAGGABLE)
+                    HStack {
+                        ZStack {
+                            Circle()
+                                .fill(Color("ColorRed"))
+                            Circle().fill(.black.opacity(0.15))
+                                .padding(8)
+                            
+                            Image(systemName: "chevron.right.2")
+                                .font(.system(size: 24, weight: .bold))
+                        }
+                        .foregroundColor(.white)
+                    .frame(width: 80, height: 80, alignment: .center)
+                    .onTapGesture {
+                        isOnboardingViewActive = false
+                    }
+                        
+                        
+                    Spacer()
+                    } //: HSTACK
+                    
+                }//: FOOTER
+                .frame(height: 80, alignment: .center)
+                .padding()
+            }//: VSTACK
+        }//: ZSTACK
     }
 }
 
